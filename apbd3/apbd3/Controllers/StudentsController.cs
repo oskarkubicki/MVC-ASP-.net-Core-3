63dd14687f6bcc38c2259311d9ff876da606133b
unknown <s19732@pjwstk.edu.pl>
2020-03-26 16:07:19 +0100

    basic api

---
+++ apbd3/apbd3/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd3.Controllers
+
+    
+{
+
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "Kowalski,Malewski,Andrzejewski";
+        }
+    }
+}
