d02ad6729d2e61b1d1fed65203291c4b3d800e58
unknown <s19732@pjwstk.edu.pl>
2020-03-26 16:16:19 +0100

    id dziala

--- apbd3/apbd3/Controllers/StudentsController.cs
+++ apbd3/apbd3/Controllers/StudentsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace apbd3.Controllers
 
-    
+
 {
 
     [ApiController]
@@ -16,6 +16,29 @@ namespace apbd3.Controllers
         public string GetStudent()
         {
             return "Kowalski,Malewski,Andrzejewski";
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+
+            if (id == 1)
+            {
+
+                return Ok("Kowalski");
+
+            }
+            else if (id == 2)
+            {
+
+                return Ok("Malewski");
+
+            }
+            else {
+
+                return NotFound("The element was not found");
+            }
         }
     }
 }
