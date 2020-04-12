3c1ad5278fa0bcb48b4b2206868ee5e5bef0558d
unknown <s19732@pjwstk.edu.pl>
2020-04-12 22:19:02 +0200

    middlewares

---
+++ apbd3/apbd3/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apbd3.Middlewares
+{
+    public class LoggingMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+
+
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+
+            _next = next;
+
+        }
+    }
+}
